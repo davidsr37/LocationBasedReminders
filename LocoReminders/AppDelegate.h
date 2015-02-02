377e530051f577e5f8ce1ed204f519fbf38eca58
davidsr37 <davidsamuelrogers@gmail.com>
2015-02-02 09:21:55 -0800

    Initial Commit

---
+++ LocoReminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocoReminders
+//
+//  Created by David Rogers on 2/2/15.
+//  Copyright (c) 2015 David Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocoReminders/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocoReminders
+//
+//  Created by David Rogers on 2/2/15.
+//  Copyright (c) 2015 David Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
